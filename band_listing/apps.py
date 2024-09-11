eb267263938d4b5a4cb710ed7bce232c4cf13a17
Craig Dickerson <craigdickerson@hotmail.com>
2024-09-11 12:51:58 +0000

    Init commit.  Create onebigdarkroom project and band listing app

---
+++ band_listing/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BandListingConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'band_listing'
---
+++ onebigdarkroom/urls.py
@@ -0,0 +1,24 @@
+"""
+URL configuration for onebigdarkroom project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from band_listing.views import my_band_listing
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('band_listing/', my_band_listing, name='band_listing'),
+]
