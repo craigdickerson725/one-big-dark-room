95f0cd6baf41d3bb55789d0d9f1746cd64ac9cc4
Craig Dickerson <craigdickerson@hotmail.com>
2024-09-12 13:41:02 +0000

    Register message model to admin site

--- band_listing/admin.py
+++ band_listing/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import BandListing
+from .models import BandListing, Message
 
 # Register your models here.
 admin.site.register(BandListing)
+admin.site.register(Message)
