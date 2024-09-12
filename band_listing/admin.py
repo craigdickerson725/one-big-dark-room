e8eafc9bd3a42f4a2ef140ac719dcb813a2006c7
Craig Dickerson <craigdickerson@hotmail.com>
2024-09-12 09:38:38 +0000

    Replace database to correct database error, set new deployment settings

--- band_listing/admin.py
+++ band_listing/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import BandListing
 
 # Register your models here.
+admin.site.register(BandListing)
--- band_listing/models.py
+++ band_listing/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
+class BandListing(models.Model):
+    band_name = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    photo = models.ImageField(upload_to='band_photos/', blank=True, null=True)
+    description = models.TextField()
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_listings")
+    created_at = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
