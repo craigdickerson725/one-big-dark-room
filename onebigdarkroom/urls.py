d209862649af71b537865be885111d2b7e911ff1
Craig Dickerson <craigdickerson@hotmail.com>
2024-09-19 07:05:51 +0000

    Add favicon for site

--- onebigdarkroom/urls.py
+++ onebigdarkroom/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-# from band_listing.views import my_band_listing
 
 urlpatterns = [
     path('admin/', admin.site.urls),
